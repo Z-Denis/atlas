091ba087d4bc0b83c2f52c33706fb688c7366887
Z-Denis <zakari.denis@gmail.com>
2026-07-11 17:15:26 +0200

    remove duplicate test

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -52,16 +52,4 @@ mod tests {
 
         assert_eq!(density.dims(), [1]);
     }
-
-    #[test]
-    fn model_log_value_shape_is_correct() {
-        let device = Default::default();
-        let space = ContinuousSpace::new(-1.0f32, 1.0, 1);
-        let samples: Tensor<NdArray, 2, Float> = Tensor::from_data([[0.0f32]], &device);
-        let model = ZeroModel;
-
-        let density = model.log_value(&space, samples);
-
-        assert_eq!(density.dims(), [1]);
-    }
 }
